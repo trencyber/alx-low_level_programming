70c22f2880968ef51abe9dd29d69eadde065b83e
trencyber <trencyber@gmail.com>
2022-12-20 23:58:41 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return:  0 (Success)
+ */
+int main(void)
+{
+	char *sh = "main";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
