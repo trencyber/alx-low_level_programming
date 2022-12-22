ecb32ff3132d7257e5eda38a63ad3b49a4000762
trencyber <trencyber@gmail.com>
2022-12-22 13:29:46 -0800

    task 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,13 @@
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int i = 0;
-        char *begin = dest;
+	int i = 0;
+	char *begin = dest;
 
-        for (; i < n && src[i] != '\0'; i++)
-                dest[i] = src[i];
-        for (; i < n; i++)
-                dest[i] = '\0';
-        return (begin);
+	for (; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (begin);
 }
 
