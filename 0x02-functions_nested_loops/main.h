f09d517653346f9c33f18ef0dc0e2b328cdcb917
trencyber <trencyber@gmail.com>
2022-12-27 13:15:15 -0800

    main file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _islower(int c);
+int print_sign(int n);
 int _putchar(char);
+int _abs(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _isalpha(int c);
+int print_last_digit(int);
 
 
 #endif
